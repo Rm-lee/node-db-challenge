f2f4e8966586691211f7d4e1f4f34f099db4f9b2
Roger Lee <rmlee0003@gmail.com>
2020-01-12 19:41:19 -0500

    created migration file

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const server = express()
 server.use(helmet())
 server.use(express.json())
 
-server.get('api/', (req,res) => {
+server.get('/api/', (req,res) => {
  res.status(200).json("working")
 })
 
 
 
-modules.exports = server
+module.exports = server
